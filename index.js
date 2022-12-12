0277621b9b711a7921694a03e08b80e95b0c4d81
fcxmarquez <francisco.marquez.solt@gmail.com>
2022-12-12 14:16:04 -0600

    Completed others services

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const routerApi = require('./routes');
 const app = express();
 const { logErrors, boomErrorHandler } = require('./middlewares/error.handler');
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 // Middleware
 app.use(express.json());
--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const productsRouter = require('./productsRouter');
 const usersRouter = require('./usersRouter');
 const categoriesRouter = require('./categoriesRouter');
+const orderRouter = require('./ordersRouter');
 
 function routerApi(app) {
   const router = express.Router();
@@ -10,6 +11,7 @@ function routerApi(app) {
   router.use('/products', productsRouter);
   router.use('/users', usersRouter);
   router.use('/categories', categoriesRouter);
+  router.use('/orders', orderRouter);
 }
 
 module.exports = routerApi;
