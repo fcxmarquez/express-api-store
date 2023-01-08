25d689fc92c82f80751e67c6b5c31356bde79782
fcxmarquez <francisco.marquez.solt@gmail.com>
2023-01-08 17:20:03 -0600

    Configurando migraciones

--- libs/sequelize.js
+++ libs/sequelize.js
@@ -4,12 +4,10 @@ const setupModels = require('../db/models');
 
 const USER = encodeURIComponent(config.dbUser);
 const PASSWORD = encodeURIComponent(config.dbPassword);
-const URI = `mysql://${USER}:${PASSWORD}@${config.dbHost}:${config.dbPort}/${config.dbName}`;
+const URI = `postgres://${USER}:${PASSWORD}@${config.dbHost}:${config.dbPort}/${config.dbName}`;
 
-const sequelize = new Sequelize(URI, { dialect: 'mysql', logging: true });
+const sequelize = new Sequelize(URI, { dialect: 'postgres', logging: true });
 
 setupModels(sequelize);
-
-sequelize.sync(); // It will create the tables if they don't exist
 
 module.exports = sequelize;
