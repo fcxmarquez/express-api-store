096fbf7d9146a8525bf8244e3e7826af75c2a461
fcxmarquez <francisco.marquez.solt@gmail.com>
2023-05-19 16:20:20 -0600

    Updated migration without virtual field

--- db/migrations/20230218002114-order.js
+++ db/migrations/20230218002114-order.js
@@ -1,10 +1,36 @@
 'use strict';
 
-const { OrderSchema, ORDER_TABLE } = require('../models/order.model');
+const { ORDER_TABLE } = require('../models/order.model');
+const { CUSTOMER_TABLE } = require('../models/customer.model');
+const { DataTypes, Sequelize } = require('sequelize');
 
 module.exports = {
   async up(queryInterface) {
-    await queryInterface.createTable(ORDER_TABLE, OrderSchema);
+    await queryInterface.createTable(ORDER_TABLE, {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: DataTypes.INTEGER,
+      },
+      customerId: {
+        field: 'customer_id',
+        allowNull: false,
+        type: DataTypes.INTEGER,
+        references: {
+          model: CUSTOMER_TABLE,
+          key: 'id',
+        },
+        onUpdate: 'CASCADE',
+        onDelete: 'SET NULL',
+      },
+      createdAt: {
+        allowNull: false,
+        type: DataTypes.DATE,
+        defaultValue: Sequelize.NOW,
+        field: 'created_at',
+      },
+    });
   },
 
   async down(queryInterface) {
