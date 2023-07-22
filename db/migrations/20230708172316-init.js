0e4bc84617f4eb193e40442b5ec15ba3fbcd979f
fcxmarquez <francisco.marquez.solt@gmail.com>
2023-07-22 14:53:09 -0600

    Reseted migrations to only have one of them

---
+++ db/migrations/20230708172316-init.js
@@ -0,0 +1,222 @@
+'use strict';
+const { DataTypes, Sequelize } = require('sequelize');
+const { USER_TABLE } = require('./../models/user.model');
+const { CUSTOMER_TABLE } = require('./../models/customer.model');
+const { CATEGORY_TABLE } = require('./../models/category.model');
+const { PRODUCT_TABLE } = require('../models/product.model');
+const { ORDER_TABLE } = require('../models/order.model');
+const { ORDER_PRODUCT_TABLE } = require('../models/order-product.model');
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface) {
+    await queryInterface.createTable(USER_TABLE, {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: DataTypes.INTEGER,
+      },
+      email: {
+        allowNull: false,
+        type: DataTypes.STRING,
+        unique: true,
+      },
+      password: {
+        allowNull: false,
+        type: DataTypes.STRING,
+      },
+      createdAt: {
+        allowNull: false,
+        type: DataTypes.DATE,
+        defaultValue: Sequelize.NOW,
+        field: 'created_at',
+      },
+      role: {
+        allowNull: false,
+        type: DataTypes.STRING,
+        defaultValue: 'customer',
+      },
+    });
+
+    await queryInterface.createTable(CUSTOMER_TABLE, {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: DataTypes.INTEGER,
+      },
+      name: {
+        allowNull: false,
+        type: DataTypes.STRING,
+      },
+      lastName: {
+        allowNull: false,
+        type: DataTypes.STRING,
+        field: 'last_name',
+      },
+      phone: {
+        allowNull: true,
+        type: DataTypes.STRING,
+      },
+      createdAt: {
+        allowNull: false,
+        type: DataTypes.DATE,
+        defaultValue: Sequelize.NOW,
+        field: 'created_at',
+      },
+      userId: {
+        field: 'user_id',
+        unique: true,
+        allowNull: false,
+        type: DataTypes.INTEGER,
+        references: {
+          model: USER_TABLE,
+          key: 'id',
+        },
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE',
+      },
+    });
+    await queryInterface.createTable(CATEGORY_TABLE, {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: DataTypes.INTEGER,
+      },
+      name: {
+        allowNull: false,
+        type: DataTypes.STRING,
+        unique: true,
+      },
+      image: {
+        allowNull: false,
+        type: DataTypes.STRING,
+      },
+      createdAt: {
+        allowNull: false,
+        type: DataTypes.DATE,
+        defaultValue: Sequelize.NOW,
+        field: 'created_at',
+      },
+    });
+    await queryInterface.createTable(PRODUCT_TABLE, {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: DataTypes.INTEGER,
+      },
+      name: {
+        allowNull: false,
+        type: DataTypes.STRING,
+      },
+      price: {
+        allowNull: false,
+        type: DataTypes.INTEGER,
+      },
+      image: {
+        allowNull: false,
+        type: DataTypes.STRING,
+      },
+      description: {
+        allowNull: false,
+        type: DataTypes.TEXT,
+      },
+      createdAt: {
+        allowNull: false,
+        type: DataTypes.DATE,
+        defaultValue: Sequelize.NOW,
+        field: 'created_at',
+      },
+      categoryId: {
+        field: 'category_id',
+        allowNull: false,
+        type: DataTypes.INTEGER,
+        references: {
+          model: CATEGORY_TABLE,
+          key: 'id',
+        },
+        onUpdate: 'CASCADE',
+        onDelete: 'SET NULL',
+      },
+    });
+    await queryInterface.createTable(ORDER_TABLE, {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: DataTypes.INTEGER,
+      },
+      customerId: {
+        field: 'customer_id',
+        allowNull: false,
+        type: DataTypes.INTEGER,
+        references: {
+          model: CUSTOMER_TABLE,
+          key: 'id',
+        },
+        onUpdate: 'CASCADE',
+        onDelete: 'SET NULL',
+      },
+      createdAt: {
+        allowNull: false,
+        type: DataTypes.DATE,
+        defaultValue: Sequelize.NOW,
+        field: 'created_at',
+      },
+    });
+    await queryInterface.createTable(ORDER_PRODUCT_TABLE, {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: DataTypes.INTEGER,
+      },
+      createdAt: {
+        allowNull: false,
+        type: DataTypes.DATE,
+        defaultValue: Sequelize.NOW,
+        field: 'created_at',
+      },
+      amount: {
+        allowNull: false,
+        type: DataTypes.INTEGER,
+      },
+      orderId: {
+        field: 'order_id',
+        allowNull: false,
+        type: DataTypes.INTEGER,
+        references: {
+          model: ORDER_TABLE,
+          key: 'id',
+        },
+        onUpdate: 'CASCADE',
+        onDelete: 'SET NULL',
+        unique: false,
+      },
+      productId: {
+        field: 'product_id',
+        allowNull: false,
+        type: DataTypes.INTEGER,
+        references: {
+          model: PRODUCT_TABLE,
+          key: 'id',
+        },
+        onUpdate: 'CASCADE',
+        onDelete: 'SET NULL',
+        unique: false,
+      },
+    });
+  },
+
+  async down(queryInterface) {
+    await queryInterface.dropTable(ORDER_PRODUCT_TABLE);
+    await queryInterface.dropTable(ORDER_TABLE);
+    await queryInterface.dropTable(PRODUCT_TABLE);
+    await queryInterface.dropTable(CATEGORY_TABLE);
+    await queryInterface.dropTable(CUSTOMER_TABLE);
+    await queryInterface.dropTable(USER_TABLE);
+  },
+};
