4b8016aefedb63a86d1fe08b7a36eb446d8b7d42
fcxmarquez <francisco.marquez.solt@gmail.com>
2023-11-22 14:17:41 -0600

    Generar JWT en el servicio

--- config/config.js
+++ config/config.js
@@ -10,6 +10,7 @@ const config = {
   dbName: process.env.DB_NAME,
   dbPort: process.env.DB_PORT,
   apiKey: process.env.API_KEY,
+  jwtSecret: process.env.JWT_SECRET,
 };
 
 module.exports = { config };
